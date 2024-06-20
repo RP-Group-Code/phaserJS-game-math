226acdd4dc6ad40c39b33753cfc9083d4e215230
RP-Group-Code <mhasurungan54@gmail.com>
2024-06-20 14:38:51 +0700

    first add phaer game math

---
+++ src/scenes/GameOverScene.js
@@ -0,0 +1,33 @@
+import Phaser from "phaser";
+export default class GameOverScene extends Phaser.Scene {
+  constructor() {
+    super("over-scene");
+  }
+  init(data) {
+    this.replayButton = undefined;
+    this.score = data.score;
+  }
+  preload() {
+    this.load.image("gameover", "images/gameover.png");
+    this.load.image("replaybutton", "images/replay.png");
+
+  }
+  create() {
+    this.add.image(240, 320, "background");
+    this.add.image(240, 320, "gameover");
+
+    /*.........................*/
+    this.add.text(100, 300, "Score: " + this.score, {
+      fontSize: "32px",
+      fill: "black",
+    });
+    this.replayButton = this.add.image(200, 400, "replay-button").setInteractive().setScale(0.5);
+    this.replayButton.once(
+      "pointerup",
+      () => {
+        this.scene.start("math-fighter-scene");
+      },
+      this
+    );
+  }
+}
